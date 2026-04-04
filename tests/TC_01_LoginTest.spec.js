1e31b5c0b7b9383978de2bf25a015f0e5b07f29d
tahmid888 <tahmidulanon@gmail.com>
2026-04-04 22:43:24 +0600

    POM-Added

---
+++ tests/TC_01_LoginTest.spec.js
@@ -0,0 +1,16 @@
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../pages/LoginPage";
+import loginData from "../testData/loginData.json";
+
+test("Login Test", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  // await login.gotoLoginPage();
+  // await login.login("a", "b@123");
+  // await page.waitForTimeout(3000);
+
+  // use first object from JSON
+  const data = loginData[0];
+
+  await loginPage.gotoLoginPage(data.url);
+  await loginPage.login(data.username, data.password);
+});
